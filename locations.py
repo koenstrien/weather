e2c17269d2bdd860dc8ddfa3bb0148df5af411bc
koenstrien <66589602+koenstrien@users.noreply.github.com>
2024-02-28 09:26:36 +0100

    Move locations to seperate file

---
+++ locations.py
@@ -0,0 +1,7 @@
+LOCATIONS = {
+    "Eindhoven": (51.44, 5.48),
+    "Romania": (47.06, 21.93),
+    "Utrecht": (52.09, 5.12),
+    "Heythuysen": (51.25, 5.89),
+    "California": (36.77, -119.41),
+}
